627af3d1a85ea39f342b07b43c2131a64153e1b1
[sadra sadeghi] <[sadra82.sadeghi@gmail.com]>
2024-06-23 14:15:24 +0330

    first touch on Remix

---
+++ app/routes/jokes.$jokeId.tsx
@@ -0,0 +1,12 @@
+export default function JokeRoute() {
+        return (
+          <div>
+            <p>Here is your hilarious joke:</p>
+            <p>
+              Why do not you find hippopotamuses hiding in trees?
+              They are really good at it.
+            </p>
+          </div>
+        );
+      }
+      
