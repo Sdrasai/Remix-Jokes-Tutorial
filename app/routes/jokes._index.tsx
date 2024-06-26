1b99475119eb67afbecdaf5c5cdf01361d447a63
[sadra sadeghi] <[sadra82.sadeghi@gmail.com]>
2024-06-26 14:21:52 +0330

    database added + routes upgraded

--- app/routes/jokes._index.tsx
+++ app/routes/jokes._index.tsx
@@ -1,12 +1,29 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    skip: randomRowNumber,
+    take: 1,
+  });
+  return json({ randomJoke });
+};
+
 export default function JokesIndexRoute() {
-        return (
-          <div>
-            <p>Here is a random joke:</p>
-            <p>
-              I was wondering why the frisbee was getting bigger,
-              then it hit me.
-            </p>
-          </div>
-        );
-      }
-      
+  const data = useLoaderData<typeof loader>();
+  // console.log("++++++++++++++++++++++++",data.randomJoke.name);
+  
+  return (
+    <div>
+      <p>Here is a random joke:</p>
+      <p>{data.randomJoke.name}</p>
+      <Link to={data.randomJoke.id}>
+        `{data.randomJoke.name}`` Permalink
+      </Link>
+    </div>
+  );
+}
